8f768442c88869483edcab89bfcebe84a6f0c13f
Lim <limhsiuying90@gmail.com>
2023-10-10 12:19:56 +0800

    add listings models

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_10_032628) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_10_040039) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "listings", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "title"
+    t.text "description"
+    t.string "property_type"
+    t.string "location"
+    t.decimal "price", precision: 10, scale: 2
+    t.integer "rooms", default: 0
+    t.integer "capacity", default: 0
+    t.boolean "available", default: false
+    t.decimal "avg_rating", precision: 5, scale: 2
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_listings_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -31,4 +47,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_10_032628) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "listings", "users"
 end
